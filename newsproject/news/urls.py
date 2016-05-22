bca2d447867aa21352f4eb00a7021d11efcffe55
vanstinger <vandykfernandes@hotmail.com>
2016-05-22 18:08:52 +0400

    Creation of urls, views and required templates using django templating

--- newsproject/news/urls.py
+++ newsproject/news/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.articles_list, name='articles_list'),
+    url(r'^feeds/new', views.feed_new, name='feed_new'),
+    url(r'^feeds/', views.feeds_list, name='feeds_list'),
 ]
--- newsproject/news/views.py
+++ newsproject/news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Article
+from .models import Article, Feed
 
 # Create your views here.
 
@@ -10,3 +10,18 @@ def articles_list(request):
         'articles': articles
     }
     return render(request, 'news/articles_list.html', context)
+
+
+def feeds_list(request):
+    feeds = Feed.objects.all
+    context = {
+        'feeds': feeds
+    }
+    return render(request, 'news/feeds_list.html', context)
+
+
+def feed_new(request):
+    context = {
+
+    }
+    return render(request, 'news/new_feed.html', context)
