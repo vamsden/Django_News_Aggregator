ef07817c919bc31247a9a92c84fc1eb33c6d624c
vanstinger <vandykfernandes@hotmail.com>
2016-05-23 12:24:21 +0400

    Installed Feed parser and applied function to get feed articles.

--- newsproject/news/views.py
+++ newsproject/news/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Article, Feed
 from .forms import FeedForm
 
+import datetime
+import feedparser
 # Create your views here.
 
 
@@ -21,13 +23,35 @@ def feeds_list(request):
     return render(request, 'news/feeds_list.html', context)
 
 
+# New feed entry form
 def feed_new(request):
     if request.method == "POST":
         form = FeedForm(request.POST)
         if form.is_valid():
             feed = form.save(commit=False)
-            feed.title = "Title"
+
+            # Feed Parser function
+            feedData = feedparser.parse(feed.url)
+
+            # Gets feed title
+            feed.title = feedData.feed.title
             feed.save()
+
+            for entry in feedData.entries:
+                article = Article()
+                article.title = entry.title
+                article.url = entry.link
+                article.description = entry.description
+
+                # Date formatting
+                d = datetime.datetime(*(entry.published_parsed[0:6]))
+                dateString = d.strftime('%Y-%m-%d %H:%M:%S')
+
+                article.publication_date = dateString
+                article.feed = feed
+                article.save()
+
+            # redirects to feed_list using redirect
             return redirect('news.views.feeds_list')
     else:
         form = FeedForm()
